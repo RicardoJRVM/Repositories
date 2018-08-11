f3c45a1c9cf0e54c0a2a5858bd6d55a404226fac
RicardoJRVM <ricardini010@hotmail.com>
2018-08-11 02:21:45 -0400

    Correcciones en la posicion
    Se aÃadio color al fondo de la tarjeta y al fondo del avatar de usuario,
    se centro el contenido y se aÃadio un layout basico

--- functions.js
+++ functions.js
@@ -3,18 +3,18 @@ __ajax("data.json")
 .done(function(usuarios){
 	var html ="";
 	for (var i in usuarios.users) { 
-		html+=	`<div class="card" style="width: 18rem;">
-				<img class="card-img-top" src="${usuarios.users[i].avatar_url}" alt="profile_pic"
-				width="260" height="260" onerror="this.src='images/icono_default.jpg';">
-				<div class="card-body">
-			    <h5 class="card-title">Ficha Tecnica número ${i}</h5>
-			    <h3>Nombre de usuario:</h3>
-			    <p class="card-text"> ${usuarios.users[i].name}</p>
-			    <h3>Nickname de usuario:</h3>
-			    <p class="card-text"> ${usuarios.users[i].nickName}</p>
-			    <h3>URL de Github:</h3>
-			    <a href="${usuarios.users[i].github_url}" class="btn btn-primary">${usuarios.users[i].github_url}</a>
-			  	</div>
+		html+=	`<div class="card" style="width: 18rem; background-color:#20B2AA;border-color:#287233; ">
+					<img class="card-img-top img-fluid" style="background-color:white;" src="${usuarios.users[i].avatar_url}" alt="profile_pic"
+					width="260" height="260" onerror="this.src='images/icono_default.jpg';">
+						<div class="card-body" >
+					    <h5 class="card-title">Ficha Tecnica ${i}</h5>
+					    <h3>Nombre de usuario:</h3>
+					    <p class="card-text"> ${usuarios.users[i].name}</p>
+					    <h3>Nickname de usuario:</h3>
+					    <p class="card-text"> ${usuarios.users[i].nickName}</p>
+					    <h3>URL de Github:</h3>
+					    <a href="${usuarios.users[i].github_url}" class="btn btn-primary" style="white-space:normal;">${usuarios.users[i].github_url}</a>
+					  	</div>
 				</div>
 				<hr>
 				`
